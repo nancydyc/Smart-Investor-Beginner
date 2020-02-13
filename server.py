dc275066b5c082adbbc32417272580ff36eb9edd
Yichen Dai <msdaiyichen@gmail.com>
2020-02-13 02:18:05 +0000

    Listed all the functions needed for feature1.0

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from model import connect_to_db, db
 
+import json
+import requests
+
 
 app = Flask(__name__)
 
@@ -25,11 +28,68 @@ def homepage():
     return render_template("home.html")
 
 
+@app.route('/')
+def search_stock_form():
+    """Search stocks by symbol or key words."""
+
+    # Get user input from the search form
+    # If the symbol is in the set of symbols/key words in the company names,
+    # return stock realtime price from Alpha Vantage API 
+    # and company name data from Edgar Online API.
+    # Else,...
+
+    pass 
+    # User Ajax to work on home.html
+
+
 @app.route('/stock')
 def display_stock():
     """Display single stock page of key data and company fundamentals."""
+
     return render_template("stock.html")
 
+
+@app.route('/stock/<stock_id>')
+def show_price_chart():
+    """Display single stock page of key data and company fundamentals."""
+
+    # Get user input from the search form
+    # If the symbol is in the set of symbols/key words in the company names,
+    # return stock realtime price and company name, daily/weekly/monthly price data
+    # from Alpha Vantage API to make a chart
+
+    pass
+
+
+@app.route('/screen')
+def screen_stocks():
+    """Stock screener, showing screening criteria."""
+
+    return render_template("screen.html")
+
+
+@app.route('/screen')
+def get_price_range():
+    """Stock screener."""
+
+    # Get price range from user
+    # 2.0 Get week numbers
+
+    # If the realtime price of the stocks in database in the price range,
+    # return a list of the company names
+    return redirect("result.html")
+
+
+@app.route('/result')
+def screen_result():
+    """Display stock screening results, showing symbol, company names and price."""
+
+    return render_template("result.html")
+
+
+
+
+####################################2.0 feature################################
 
 @app.route('/compare')
 def compare_stocks():
@@ -48,17 +108,6 @@ def register():
     """New member register."""
     return render_template("register.html")
 
-
-@app.route('/screen')
-def screen_stocks():
-    """Stock screener."""
-    return render_template("screen.html")
-
-
-@app.route('/result')
-def screen_result():
-    """Display the results of stocks screening."""
-    return render_template("result.html")
 
 ###############################################################################
 if __name__ == "__main__":
